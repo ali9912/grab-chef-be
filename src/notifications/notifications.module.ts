bd731cdea332689da0e5d1398ec89c9e06d2fa4c
Insaram Alam Siddique <alaminsaram92@gmail.com>
2025-06-16 23:24:57 +0500

    notifications cert enabled

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -5,7 +5,7 @@ import * as admin from 'firebase-admin';
 import { NotificationController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { NotificationsSchema } from './schemas/notification.schema';
-// var serviceAccount = require('../../grab-chef-service.json');
+var serviceAccount = require('../../grab-chef-service.json');
 
 config();
 
@@ -30,7 +30,7 @@ export class NotificationsModule {
       //   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       //   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       // }),
-      // credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount),
     });
   }
 }
